7c23cb1280fd1845631d3757036b9afa5241dad7
ParfeLatte <128596001+ParfeLatte@users.noreply.github.com>
2023-04-03 23:31:55 +0900

    공격 로직 완성 및 몬스터 사망 구현
    플레이어와 기본 몬스터 모두 공격 구현이 끝났으며, 몬스터의 경우에는 사망까지 구현했습니다.

--- Assets/ParfeLatte/Monster.cs
+++ Assets/ParfeLatte/Monster.cs
@@ -7,33 +7,40 @@ public class Monster : LivingEntity
     //public GameObject small;//범위 15의 콜라이더 영역
     //public GameObject Big;//범위 30의 콜라이더 영역
     public GameObject Player;//거리측정용
+    public Transform AttackPos;//공격범위 위치
+    public Vector2 boxSize;//공격범위
 
     public int SleepState;//현재 수면상태 0:깊은수면, 1:중간수면, 2:얕은수면, 3:기상!!!
 
     public float moveTimeOne;//플레이어가 얼마나 걸었는지 확인
     public float moveTimeTwo;//플레이어가 얼마나 걸었는지 확인
     public float CheckTime;//범위 안에 들어온 시간
+    public float CoolTime;//공격 쿨타임
+    public float AttackTime;//공격시간
     public float Dist;//몬스터와 플레이어 사이 거리
     public float Dir;//이동방향
     public Vector3 dirVec;//레이 방향
 
     public Player player;//플레이어 코드
+    //public MonsterAttack Attack;//공격 실행 스크립트
 
     public bool isPlayerDash;//플레이어가 대쉬했는지 확인
     public bool isPlayerStop;//플레이어가 멈췄는가?
 
+    public Vector3 lastPlayerPosition;//위치 비교용
 
-    public Vector3 lastPlayerPosition;//
 
     private Rigidbody2D MR;
     private Animator animator;
     private SpriteRenderer MonsterRenderer;
 
-    private Vector3 curPos;
+    private Vector3 curPos;//현재위치
+    private Vector2 AddPos = new Vector2(0, 0.5f);//pivot을 아래로 고정했으므로 레이 검사때 위로
+    private Vector2 RayPos;
+    private GameObject AttackPlayer;//
 
-    private GameObject AttackPlayer;
+    private bool isMobMove = false;//몬스터가 움직이는지
 
-    private bool isAttack = false;
     void Awake()
     {
         MR = GetComponent<Rigidbody2D>();
@@ -41,7 +48,7 @@ public class Monster : LivingEntity
         MonsterRenderer = GetComponent<SpriteRenderer>();
         SleepState = 0;
         SetStatus(31, 10, 4.5f);//일단 일반몹기준
-        Health = MaxHealth;
+        Health = MaxHealth;//체력을 최대체력으로 설정 
     }
 
     // Update is called once per frame
@@ -49,34 +56,57 @@ public class Monster : LivingEntity
     {
         Dist = Mathf.Abs(Player.transform.position.x - gameObject.transform.position.x);//플레이어와 몬스터 사이 거리
         curPos = transform.position;//현재위치
-        StateCheck();
-        OnWake();
+        RayPos = MR.position + AddPos;
+        if(SleepState != 3 & !isDead) { 
+            StateCheck();
+        }
+        if(SleepState == 3 && !isDead) {
+            OnWake();
+            AttackCheck();
+        }
+        AttackTime += Time.deltaTime;
+    }
 
-
-        RaycastHit2D rayHit = Physics2D.Raycast(MR.position, dirVec, 1.2f, LayerMask.GetMask("Player"));
+    private void AttackCheck()
+    {
+        RaycastHit2D rayHit = Physics2D.Raycast(RayPos, dirVec, 1.4f, LayerMask.GetMask("Player"));
         if (rayHit.collider != null)
         {
-            LivingEntity player = rayHit.collider.GetComponent<Player>();
+            Player player = rayHit.collider.GetComponent<Player>();
             AttackPlayer = rayHit.collider.gameObject;
             Debug.Log("플레이어가 사정거리내에 있음");
-            if (AttackPlayer != null && !isAttack)
+            if (AttackPlayer != null && AttackTime >= CoolTime)
             {
-                isAttack = true;
-                player.damaged(damage);
-                isAttack = false;
+                isMobMove = false;
+                animator.SetBool("isMove", false);
+                animator.SetTrigger("Attack");
+                Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(AttackPos.position, boxSize, 0);
+                foreach (Collider2D collider in collider2Ds)
+                {
+                    if (collider.tag == "Player")
+                    {
+                        player.damaged(damage);
+                    }
+                }
+                AttackTime = 0;
+                Invoke("MoveAgain", 0.15f); ;
             }
-            Debug.Log("Hit!");
+            Debug.Log("공격합니다.");
         }
         else
         {
             AttackPlayer = null;
             Debug.Log("사정거리내에 아무도 없음");
-            Debug.Log("Miss!");
+            Debug.Log("계속 추적합니다");
         }
+    }//공격
 
-
+    public void MoveAgain()
+    {
+        isMobMove = true;
+        animator.SetBool("isMove", true);
     }
-    
+
     private void StateCheck()
     {
         if (Dist <= 30 && SleepState != 3)
@@ -103,11 +133,9 @@ public class Monster : LivingEntity
     }
     private void OnWake()
     {
-        if (SleepState == 3)
+        Debug.DrawRay(RayPos, dirVec * 1.4f, new Color(0, 1, 0));//레이캐스트 표시(거리 확인용)
+        if (isMobMove == true)
         {
-            
-            Debug.DrawRay(MR.position, dirVec * 1.2f, new Color(0, 1, 0));//레이캐스트 표시(거리 확인용)
-            animator.SetBool("isMove", true);
             if (player.transform.position.x - gameObject.transform.position.x >= 0)
             {
                 Dir = 1;
@@ -120,12 +148,10 @@ public class Monster : LivingEntity
                 dirVec = new Vector3(-1, 0f, 0f);
                 MonsterRenderer.flipX = true;
             }
-
-
             Vector3 NextPos = new Vector3(Dir, 0, 0) * MaxSpeed * Time.deltaTime;
             transform.position = curPos + NextPos;
         }
-    }
+    }//깨어있는 동안 행동하는 루틴
 
     IEnumerator CheckStop()
     {
@@ -184,9 +210,7 @@ public class Monster : LivingEntity
                 {
                     if (player.GetMoveCheck())
                     {
-                        SleepState = 3;
-                        animator.SetInteger("SleepState", 3);
-                        Debug.Log("깨어났습니다.");
+                        MonsterAwake();//기상
                     }
                     else
                     {
@@ -203,11 +227,19 @@ public class Monster : LivingEntity
     {
         if (player.GetDashCheck() == true)
         {
-            SleepState = 3;
-            animator.SetInteger("SleepState", 3);
-            Debug.Log("깨어났습니다.");
+            MonsterAwake();//기상
         }
     }//플레이어가 대쉬했는지 체크함
+
+    private void MonsterAwake()
+    {
+        SleepState = 3;
+        animator.SetInteger("SleepState", 3);
+        animator.SetBool("isMove", true);
+        isMobMove = true;
+        Debug.Log("깨어났습니다.");
+    }
+
     public override void damaged(float damage)
     {
         Health -= damage;//체력에서 데미지만큼 깎음
@@ -222,6 +254,18 @@ public class Monster : LivingEntity
         //피격시 실행할 내용
     }
 
+    public override void Die()
+    {
+        isMobMove = false;
+        isDead = true;
+        animator.SetTrigger("Die");
+        Invoke("Destroy", 1.43f);
+    }
+
+    private void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
 }
 /*플레이어와 거리가 30이하가 된다  -> 검사시작 -> 2초간 걸으면 중간수면
 				          -> 대쉬하면 바로 기상
@@ -229,22 +273,23 @@ public class Monster : LivingEntity
 ->  아니면 다시  깊은 수면으로*/
 //초->노->빨->빨(유지)
 
-//RaycastHit2D rayHit = Physics2D.Raycast(PR.position, dirVec, 1.2f, LayerMask.GetMask("Enemy"));
+//RaycastHit2D rayHit = Physics2D.Raycast(MR.position, dirVec, 1.2f, LayerMask.GetMask("Player"));
 //if (rayHit.collider != null)
 //{
-//    LivingEntity enemy = rayHit.collider.GetComponent<Monster>();
-//    Enemy = rayHit.collider.gameObject;
-//    Debug.Log("몬스터가 사정거리내에 있음");
-//    if (Enemy != null)
+//    LivingEntity player = rayHit.collider.GetComponent<Player>();
+//    AttackPlayer = rayHit.collider.gameObject;
+//    Debug.Log("플레이어가 사정거리내에 있음");
+//    if (AttackPlayer != null && !isAttack)
 //    {
-//        enemy.damaged(damage);
+//        isAttack = true;
+//        player.damaged(damage);
+//        isAttack = false;
 //    }
-//    Debug.Log("Hit!");
+//    Debug.Log("공격합니다.");
 //}
 //else
 //{
-//    Enemy = null;
+//    AttackPlayer = null;
 //    Debug.Log("사정거리내에 아무도 없음");
-//    Debug.Log("Miss!");
+//    Debug.Log("계속 추적합니다");
 //}
-//수정해서 몬스터 공격 로직으로 사용
