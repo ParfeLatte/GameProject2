1114de9102652d763660c273337c056ed0fb4ab5
ParfeLatte <128596001+ParfeLatte@users.noreply.github.com>
2023-05-26 00:13:57 +0900

    UI작업
    메인화면, 인게임 일시정지 추가

--- Assets/ParfeLatte/TargetObject.cs
+++ Assets/ParfeLatte/TargetObject.cs
@@ -6,6 +6,18 @@ public class TargetObject : MonoBehaviour
 {
     public GameManager GameMgr;
 
+    private void Awake()
+    {
+        if (GameMgr.isTarget)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag == "Player")
