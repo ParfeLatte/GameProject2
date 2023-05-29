b831a6cfa8ec583071353f9adc85fb17ff38e1bd
ParfeLatte <128596001+ParfeLatte@users.noreply.github.com>
2023-05-30 02:24:42 +0900

    상호작용 오브젝트 UI 표시 및 라이트 조정
    빛이 벽을 넘기는 현상 레이어 줘서 수정

--- Assets/ParfeLatte/InteractObj.cs
+++ Assets/ParfeLatte/InteractObj.cs
@@ -5,17 +5,54 @@ using UnityEngine;
 public class InteractObj : MonoBehaviour
 {
     public GameObject InteractKeyUI;
-    
-    // Start is called before the first frame update
-    void Start()
+    public bool isInteract;
+    public bool isMoveObj;//리프트인지 엘베인지 확인
+
+    public LiftTest Lift;
+    public Elevator Elev;
+
+    private void Awake()
     {
-        
+        CheckLift();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isInteract) return;
+        if (isMoveObj) return;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Interact();
+        }
+    }
+
+    public void ShowInteractUI()
+    {
+        InteractKeyUI.SetActive(true);
+    }
+
+    public void HideInteractUI()
+    {
+        InteractKeyUI.SetActive(false);
+    }
+
+    public void Interact()
+    {
+        InteractKeyUI.SetActive(false);
+        Invoke("ShowInteractUI", 0.5f);
+    }
+
+    public void CheckLift()
+    {
+        if(Elev != null || Lift != null)
+        {
+            isMoveObj = true;
+        }
+        else if(Elev == null && Lift == null)
+        {
+            isMoveObj = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -23,6 +60,7 @@ public class InteractObj : MonoBehaviour
         if (col.tag == "Player")
         {
             InteractKeyUI.SetActive(true);
+            isInteract = true;
         }
     }
 
@@ -31,6 +69,7 @@ public class InteractObj : MonoBehaviour
         if (col.tag == "Player")
         {
             InteractKeyUI.SetActive(false);
+            isInteract = false;
         }
     }
 }
--- Assets/ParfeLatte/Lift/ElevatorButton.cs
+++ Assets/ParfeLatte/Lift/ElevatorButton.cs
@@ -7,13 +7,21 @@ public class ElevatorButton : Interactable
 {
     public Elevator elevator;
     public GameObject MoveInform;
+    public InteractObj Fkey;
     public bool isKey;
 
     private void Update()
     {
         if (isKey)
         {
-            CheckUpDown();
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                ElevUp();
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                ElevDown();
+            }
         }
     }
 
@@ -22,22 +30,36 @@ public class ElevatorButton : Interactable
         if (!elevator.isMove && !isKey)
         {
             isKey = true;
+            Fkey.HideInteractUI();
             MoveInform.SetActive(true);
         }
     }
 
+    private void ElevUp()
+    {
+        isKey = false;
+        elevator.Up();
+        MoveInform.SetActive(false);
+    }
+    
+    private void ElevDown()
+    {
+        isKey = false;
+        elevator.Down();
+        MoveInform.SetActive(false);
+    }
     private void CheckUpDown()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            elevator.Up();
             isKey = false;
+            elevator.Up();
             MoveInform.SetActive(false);
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            elevator.Down();
             isKey = false;
+            elevator.Down();
             MoveInform.SetActive(false);
         }
         Debug.Log("W키를 눌러 위로, S키를 눌러 아래로 이동하세요");
